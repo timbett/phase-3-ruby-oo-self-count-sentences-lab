ce47478808456b9acad8c1103bbb7ede80fe6c08
Timothy <tkkibet13@gmail.com>
2022-10-23 12:53:29 +0300

    Assigniment completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count=self.split(/[.?!]+/).count
+    if count==0
+      return 0
+    else
+      return count
+    end
   end
 end
